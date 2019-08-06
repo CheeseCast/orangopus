5f5f937f27225777b8074ec6581e0d48c001c2f0
CheesePwn <45247477+CheesePwn@users.noreply.github.com>
2019-08-06 01:39:26 +0100

    Projects and Collective section
    Added project information pulled from GitHub's API
    Added a collective section with images of recent backers.

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() { 
+    var github = "https://api.github.com/users/orangopus/repos"
+
+    $.get(github, function(result) {
+
+        $("#name1").append(result[5].name);
+        $("#name2").append(result[4].name);
+        $("#name3").append(result[3].name);
+        $("#name4").append(result[2].name);
+        $("#name5").append(result[1].name);
+        $("#name6").append(result[0].name);
+
+        $("#desc1").append(result[5].description);
+        $("#desc2").append(result[4].description);
+        $("#desc3").append(result[3].description);
+        $("#desc4").append(result[2].description);
+        $("#desc5").append(result[1].description);
+        $("#desc6").append(result[0].description);
+    });
+});
